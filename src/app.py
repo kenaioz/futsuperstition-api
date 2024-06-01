3a416cf1979ac18ec2a1564447a76463e97c962d
kenaioz <silvio.cesarrx@hotmail.com>
2024-06-01 01:29:39 -0300

    db connection

--- src/app.py
+++ src/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, json
+from werkzeug.exceptions import HTTPException
+
 from src.routes.pong import ping
+from src.routes.jerseys import jerseys
+
+from database.config import Config
+from database.connection import db
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_blueprint(ping)
+    app.url_map.strict_slashes = False
+    app.json.sort_keys = False
+
+    blueprints = [ping, jerseys]
+
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
+
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        response = e.get_response()
+        response.data = json.dumps(
+            {
+                "error": {
+                    "code": e.code,
+                    "name": e.name,
+                    "description": e.description,
+                }
+            }
+        )
+        print(response.data)
+        response.content_type = "application/json"
+        return response
+
     return app
 
 
